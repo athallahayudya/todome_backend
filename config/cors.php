24f6d83b5a9875b836cde67fa53dc97d6a339fa9
SeptianTito123 <septiantito123@gmail.com>
2025-11-17 16:02:30 +0700

    fix: Configure CORS to allow Flutter Web requests
    - Update 'config/cors.php' to allow all origins ('*') and all headers.
    - This resolves the browser blocking issues when accessing the API from localhost (Flutter Web).

--- config/cors.php
+++ config/cors.php
@@ -1,34 +1,12 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_methods' => ['*'], // Izinkan semua Method
+    'allowed_origins' => ['*'], // PENTING: Izinkan semua domain (termasuk localhost web)
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['*'], // PENTING: Izinkan semua header
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
-
-];
+];  
